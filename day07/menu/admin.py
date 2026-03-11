524bd5442fc067ab8c3249cfd82d3fbc8a63531c
Mahesh Raj Lamsal <maheshrajlamsal01@gmail.com>
2026-03-11 23:28:16 +0545

    worked with models.py register my models in admin

--- day07/menu/admin.py
+++ day07/menu/admin.py
@@ -1,4 +1,32 @@
 from django.contrib import admin
-from .models import ChaiVariety
-# Register your models here.
-admin.site.register(ChaiVariety)
+from .models import ChaiVariety, ChaiReview, Store, chai_certificate
+
+
+# Register ChaiVariety model
+@admin.register(ChaiVariety)
+class ChaiVarietyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'type', 'created_at')
+    list_filter = ('type', 'created_at')
+    search_fields = ('name',)
+
+
+# Register ChaiReview model
+@admin.register(ChaiReview)
+class ChaiReviewAdmin(admin.ModelAdmin):
+    list_display = ('chai', 'user', 'ratings', 'date_added')
+    list_filter = ('ratings', 'date_added')
+    search_fields = ('user__username', 'chai__name')
+
+
+# Register Store model
+@admin.register(Store)
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+    search_fields = ('name', 'location')
+
+
+# Register chai_certificate model
+@admin.register(chai_certificate)
+class ChaiCertificateAdmin(admin.ModelAdmin):
+    list_display = ('certificate_number', 'chai', 'issued_date', 'validate')
+    search_fields = ('certificate_number',)
